f8e93d4e31322a6521f3cbb7909b7f1f15ab55b0
Thinh <tanthinhdana@gmail.com>
2025-10-12 20:55:23 +0700

    Init model user & relationships

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserProfile extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'user_profile';
+
+    protected $fillable = [
+        'address',
+        'phoneNumber',
+        'userId',
+    ];
+
+    const DELETED_AT = 'deletedAt';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
